6be6eb3804ba8665516ce29704b42dbe459d820e
arpan13 <arpankumarpal728@gmail.com>
2020-07-26 21:52:47 +0530

    Updates

---
+++ precache-manifest.176a727ae2dc7f8ee4591fad15a573ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d403eb6f6bd3e8f7cec4f47dcb70b41",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "6da1c590627e64b3541b",
+    "url": "/test/static/js/2.f2072842.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.f2072842.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13e37ea9fb2630848edd",
+    "url": "/test/static/js/main.95e511e7.chunk.js"
+  },
+  {
+    "revision": "6b77482e3b7317bd30e4",
+    "url": "/test/static/js/runtime-main.329c146a.js"
+  }
+]);
